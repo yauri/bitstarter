5c5ade749fa91c4acf15a7302bc4e41a740f29f6
yauri <yauri@live.com>
2013-07-14 14:22:35 +0800

    using readfilesync and buffer

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+  var fs = require('fs');
+  
+  var data = fs.readFileSync('index.html');
+  var buffer = new Buffer(data.toString());
+  response.send(buffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
